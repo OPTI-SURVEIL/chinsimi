02e3d87c9b39c51be7859e24f323691de89220ac
pcollender <pcollender@berkeley.edu>
2018-11-07 17:42:19 -0800

    added option to just retain original pinyin

--- R/rev_py.R
+++ R/rev_py.R
@@ -4,29 +4,43 @@
 #' @param n A string or vector of strings. Should be Chinese names.
 #' @param all A logical indicator. If set to TRUE, all possible names are
 #'   returned. If false, only the first character match is returned.
+#' @param py A logical indicator. If set to TRUE, the original pinyin element is
+#'   returned instead of possible characters
 #' @return A list of parsed names
 #'
 #' @examples
 #' #original name: chen1溜
 #' revpy('{chen1,[嗔,諃,琛,賝,謓,抻,郴,瞋,捵,瘨,縝,綝,棽]}溜', all=F)
-#' revpy('{chen1,[嗔,諃,琛,賝,謓,抻,郴,瞋,捵,瘨,縝,綝,棽]}溜', all=F)
+#' revpy('{chen1,[嗔,諃,琛,賝,謓,抻,郴,瞋,捵,瘨,縝,綝,棽]}溜', all=T)
+#' revpy('{chen1,[嗔,諃,琛,賝,謓,抻,郴,瞋,捵,瘨,縝,綝,棽]}溜', py=T)
 
-revpy = function(n,all=F){
+revpy = function(n,all=F,py=F){
   todo = grep('\\{.*?\\}',n)
-  lapply(n[todo],function(n_){
-    matches = regmatches(n_,gregexpr('\\{.*?\\}',n_))[[1]]
-    combns = as.list(strsplit(n_,'\\{.*?\\}')[[1]])
-    repl = which(sapply(combns,nchar)==0)
-    pymatches = regmatches(matches,gregexpr('\\[.*?\\]',matches))
-    clist = lapply(pymatches, function(str) {
-      gsub('\\[|\\]','',strsplit(str,',')[[1]])
+  if(py){
+    lapply(n[todo],function(n_){
+      matches = regmatches(n_,gregexpr('\\{.*?\\}',n_))[[1]]
+      combns = as.list(strsplit(n_,'\\{.*?\\}')[[1]])
+      repl = which(sapply(combns,nchar)==0)
+      matches = gsub('[[:punct:]]','',gsub('\\[.*?\\]','',matches))
+      combns[sapply(combns,nchar)==0] <- matches
+      paste0(combns,collapse='')
     })
-    combns[repl] = clist
-    if(all){
-      apply(expand.grid(combns),1,paste0,collapse='')
-    }else{
-      paste0(sapply(combns,'[[',1),collapse='')
-    }
-  })
+  }else{
+    lapply(n[todo],function(n_){
+      matches = regmatches(n_,gregexpr('\\{.*?\\}',n_))[[1]]
+      combns = as.list(strsplit(n_,'\\{.*?\\}')[[1]])
+      repl = which(sapply(combns,nchar)==0)
+      pymatches = regmatches(matches,gregexpr('\\[.*?\\]',matches))
+      clist = lapply(pymatches, function(str) {
+        gsub('\\[|\\]','',strsplit(str,',')[[1]])
+      })
+      combns[repl] = clist
+      if(all){
+        apply(expand.grid(combns),1,paste0,collapse='')
+      }else{
+        paste0(sapply(combns,'[[',1),collapse='')
+      }
+    })
+  }
 }
 
