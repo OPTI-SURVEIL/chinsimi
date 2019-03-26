f9523eb2684676a5f487633298f3f0905892354d
pcollender <pcollender@berkeley.edu>
2019-03-25 17:05:05 -0700

    chasing bugs

--- R/ambig_count.R
+++ R/ambig_count.R
@@ -7,14 +7,14 @@
 #'ambiguous or multiple name information is included in the field;
 
 
-ambig_count = function(n){
-  #n[grep('\\{.*?\\}',n)] = unlist(revpy(n))
-  qmark_count = stringr::str_count(n,'[？?]')
-  paren_count = stringr::str_count(n,'\\(.*?\\)')
-  aka_count = stringr::str_count(n,'(又名)|(法名)|(真名)|(用名)|(别名)|(笔名)')
-
-  or_count = grepl('又',n) * (nchar(gsub('又.*','',n[1])) > 1) * (nchar(gsub('.*又','',n[1])) > 1)
-
-  qmark_count + paren_count + aka_count + or_count
-}
+# ambig_count = function(n){
+#   #n[grep('\\{.*?\\}',n)] = unlist(revpy(n))
+#   qmark_count = stringr::str_count(n,'[？?]')
+#   paren_count = stringr::str_count(n,'\\(.*?\\)')
+#   aka_count = stringr::str_count(n,'(又名)|(法名)|(真名)|(用名)|(别名)|(笔名)')
+#
+#   or_count = grepl('又',n) * (nchar(gsub('又.*','',n[1])) > 1) * (nchar(gsub('.*又','',n[1])) > 1)
+#
+#   qmark_count + paren_count + aka_count + or_count
+# }
 
