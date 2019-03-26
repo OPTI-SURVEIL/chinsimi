2d3fa6c23375b6bfbdd7ebfec63ad15ffcd15be4
pcollender <pcollender@berkeley.edu>
2019-03-25 17:14:32 -0700

    fixed encoding bugs?

--- R/ambig_count.R
+++ R/ambig_count.R
@@ -3,18 +3,21 @@
 #'@param n A string or vector of strings. Should be Chinese names.
 #'@return An integer vector of the same length as n
 #'@details ambig_count counts for open-closed parentheses pairs,
-#'question marks, and strings such as "又名" which may indicate that
+#'question marks, and strings such as "\\u53c8\\u540d" which may indicate that
 #'ambiguous or multiple name information is included in the field;
 
 
-ambig_count = function(n){
+ambig_count = function(n) {
   #n[grep('\\{.*?\\}',n)] = unlist(revpy(n))
-  qmark_count = stringr::str_count(n,'[？?]')
-  paren_count = stringr::str_count(n,'\\(.*?\\)')
-  aka_count = stringr::str_count(n,'(又名)|(法名)|(真名)|(用名)|(别名)|(笔名)')
+  qmark_count = stringr::str_count(n, '[\\uff1f?]')
+  paren_count = stringr::str_count(n, '\\(.*?\\)')
+  aka_count
+  = stringr::str_count(
+    n,
+    '(\\u53c8\\u540d)|(\\u6cd5\\u540d)|(\\u771f\\u540d)|(\\u7528\\u540d)|(\\u522b\\u540d)|(\\u7b14\\u540d)'
+  )
 
-  or_count = grepl('又',n) * (nchar(gsub('又.*','',n[1])) > 1) * (nchar(gsub('.*又','',n[1])) > 1)
+  or_count = grepl('\\u53c8', n) * (nchar(gsub('\\u53c8.*', '', n[1])) > 1) * (nchar(gsub('.*\\u53c8', '', n[1])) > 1)
 
   qmark_count + paren_count + aka_count + or_count
 }
-
