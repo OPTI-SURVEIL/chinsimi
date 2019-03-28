3e0b6342f628911fcc68fc3736c0eb84ac2e1124
pcollender <pcollender@berkeley.edu>
2019-03-27 18:25:05 -0700

    converted lookup tables to UTF-8

--- R/name_freq.R
+++ R/name_freq.R
@@ -17,7 +17,6 @@ name_freq_table = function(refdata,start = 1, end= 9999){
          Darwin = Sys.setlocale(locale = 'en_US.UTF-8'),
          Windows = Sys.setlocale(locale = 'chs'))
 
-
   if(!any(class(refdata) == 'environment')){
     ns = substr(refdata,start,end)
     ns[nchar(ns)==0] = NA
@@ -36,7 +35,6 @@ name_freq = function(n,refdata,start = 1,end = 9999, log = T){
   switch(OS, Linux = Sys.setlocale(locale = 'en_US.UTF-8'),
          Darwin = Sys.setlocale(locale = 'en_US.UTF-8'),
          Windows = Sys.setlocale(locale = 'chs'))
-
   if(!any(class(refdata) == 'environment')) refdata = name_freq_table(refdata, start, end)
 
   ns = substr(n,start,end)
