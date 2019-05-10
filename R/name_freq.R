e4b0216857f9f6e2ce9b682b3cbe08f9d5cf809a
pcollender <pcollender@berkeley.edu>
2019-05-10 13:58:36 -0700

    further refinements to data.table implementation

--- R/name_freq.R
+++ R/name_freq.R
@@ -27,7 +27,7 @@ name_freq_table = function(refdata,start = 1, end= 9999){
   #   }
   # }
 
-  if(!any(class(refdata) == 'environment')){
+  if(!any(class(refdata) == 'data.table')){
     enc = Encoding(refdata)
     if(!all(enc == 'UTF-8')) refdata = enc2utf8(refdata)
     ns = substr(refdata,start,end)
@@ -36,9 +36,8 @@ name_freq_table = function(refdata,start = 1, end= 9999){
     ndef = sum(!is.na(ns))
     refdata = refdata[!is.na(nm)]
     refdata[,freq:= .N/ndef, by = nm]
-    temp = as.list(refdata[,freq])
-    names(temp) = refdata[,nm]
-    refdata = list2env(temp)
+    set.key(refdata,nm)
+    refdata = unique(refdata)
   }
   refdata
 }
@@ -60,49 +59,50 @@ name_freq = function(n,refdata,start = 1,end = 9999, log = T){
   #   }
   # }
 
-  if(!any(class(refdata) == 'environment')) refdata = name_freq_table(refdata, start, end)
+  if(!any(class(refdata) == 'data.table')) refdata = name_freq_table(refdata, start, end)
 
   ns = substr(n,start,end)
   ns[nchar(ns)==0] = NA
 
-  res = unlist(mget(ns, refdata, ifnotfound = 0))
-  res[is.na(names(res))] = NA
+  res = refdata[match(ns,nm)]
 
-  if(log) log(res) else res
+  freqs = res$freq
+  names(freqs) = res$nm
+  if(log) log(freqs) else freqs
 }
 
-name_freq_compare = function(n1,n2,refdata,start = 1,end = 9999, log = T){
-
-  # OS = Sys.info()['sysname']
-  # switch(OS, Linux = Sys.setlocale(locale = 'zh_CN.GBK'),
-  #        Darwin = Sys.setlocale(locale = 'zh_CN.GBK'),
-  #        Windows = Sys.setlocale(locale = 'chs'))
-
-  # if(any(class(refdata) == 'environment')){
-  #   enc = Encoding(names(refdata))
-  #   if(!all(enc == 'UTF-8')){
-  #     nms = enc2utf8(names(refdata))
-  #     refdata= as.list(refdata)
-  #     names(refdata) = nms
-  #     refdata = list2env(refdata)
-  #   }
-  # }
-
-  if(!any(class(refdata) == 'environment')) refdata = name_freq_table(refdata, start, end)
-
-  ns1 = substr(n1,start,end)
-  ns1[nchar(ns1)==0] = NA
-
-  ns2 = substr(n2,start,end)
-  ns2[nchar(ns2)==0] = NA
-
-  doinds = which(ns1==ns2)
-
-  res = rep(NA,length(ns1))
-
-  res[doinds] = unlist(mget(ns1[doinds], refdata, ifnotfound = 0))
-
-  if(log) log(res) else res
-}
-
-
+# name_freq_compare = function(n1,n2,refdata,start = 1,end = 9999, log = T){
+#
+#   # OS = Sys.info()['sysname']
+#   # switch(OS, Linux = Sys.setlocale(locale = 'zh_CN.GBK'),
+#   #        Darwin = Sys.setlocale(locale = 'zh_CN.GBK'),
+#   #        Windows = Sys.setlocale(locale = 'chs'))
+#
+#   # if(any(class(refdata) == 'environment')){
+#   #   enc = Encoding(names(refdata))
+#   #   if(!all(enc == 'UTF-8')){
+#   #     nms = enc2utf8(names(refdata))
+#   #     refdata= as.list(refdata)
+#   #     names(refdata) = nms
+#   #     refdata = list2env(refdata)
+#   #   }
+#   # }
+#
+#   if(!any(class(refdata) == 'data.table')) refdata = name_freq_table(refdata, start, end)
+#
+#   ns1 = substr(n1,start,end)
+#   ns1[nchar(ns1)==0] = NA
+#
+#   ns2 = substr(n2,start,end)
+#   ns2[nchar(ns2)==0] = NA
+#
+#   doinds = which(ns1==ns2)
+#
+#   res = rep(NA,length(ns1))
+#
+#   res[doinds] = unlist(mget(ns1[doinds], refdata, ifnotfound = 0))
+#
+#   if(log) log(res) else res
+# }
+#
+#
